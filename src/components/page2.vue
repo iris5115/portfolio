d165f8a8f83a8ef06c6817abe9aa2519be7eaaf9
iris5115 <yukurio5115iris@gmail.com>
2020-03-19 20:07:53 +0900

    create page layout
    仮で動くSPAページの土台作成

---
+++ src/components/page2.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>ここはトップ2ページです。</p>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/components/page3.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>ここはトップ3ページです。</p>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
